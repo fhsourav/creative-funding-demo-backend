9eb5b67f711ca65ab7d3b31a8b529c329a337178
fhsourav <69208613+fhsourav@users.noreply.github.com>
2025-10-18 12:56:23 +0600

    feat: Implement persistence for User and Role entities

---
+++ src/main/java/com/acescribbles/creative_funding_demo/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.acescribbles.creative_funding_demo.repositories;
+
+import com.acescribbles.creative_funding_demo.domain.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	Optional<User> findUserByEmail(String email);
+
+}
